f21768864569ddd19de9df988fb153887b9904f4
Luceta <57941164+Luceta@users.noreply.github.com>
2021-01-01 12:21:25 +0900

    ADD : room admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -16,6 +16,61 @@ class ItemAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
     """ Room Model Definition """
 
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "description",
+        "country",
+        "city",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    ordering = ("price",)
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",  # __ 2번으로 superhost를 필터링
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    # 외부 서치바
+    search_fields = ("=city", "^host__username")
+
+    filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    # roomadmin 이 self, obj는 row를 받음 이함수를 리스트에 넣음으로 정렬에 갯수 표시가능
+    def count_amenities(self, obj):
+        return "potato"
+        # count_amenities.short_descrtiption = " "이렇게 함으로써 컬럼명도 변경 가능
+
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
