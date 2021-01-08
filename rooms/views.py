818977e63b4ed8f3604b2ebe29e0c549c9f9ecdc
Luceta <57941164+Luceta@users.noreply.github.com>
2021-01-08 20:43:32 +0900

    ADD: Home view with python code without django. just use template

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . import models
+from math import ceil
 
 # Create your views here.
 
@@ -9,5 +10,21 @@ from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
-    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    page_size = 10
+    limit = page * page_size
+    offset = limit - page_size
+    page_count = ceil(models.Room.objects.count() / 10)
+    print(page_count)
+    all_rooms = models.Room.objects.all()[offset:limit]
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(0, page_count),
+        },
+    )
