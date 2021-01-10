76efe6e017867ac36af8138d2b78569381f16165
Luceta <57941164+Luceta@users.noreply.github.com>
2021-01-10 15:37:18 +0900

    Revised Home view from paginator to CBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,21 +1,20 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView
 from . import models
-from math import ceil
-
-# Create your views here.
 
 # template 네임과 return html 이름 동일 해야함
 # view name 은 urls.py파일에 있는 이름과 같아야함 all_rooms
 # template에서 context변수와 같아야함.
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", {"page": rooms})
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
+    pass
+    """home detailed view"""
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    page_kwarg = "page"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
